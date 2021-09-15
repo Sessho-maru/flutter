5b89371f55adaa46fc637cfe1a7efe951c62a6e9
AprilGong <volero458@gmail.com>
2021-09-15 23:17:18 +0900

    simple grid style gallery suppors sorting(shuffle, reverse) \n TODO:implementing bottom action buttons

--- lib/MyScreen.dart
+++ lib/MyScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:material_dialogs/material_dialogs.dart';
 import 'dart:async';
+import 'dart:math';
 
 class MyPicView extends StatelessWidget{
   final String src;
@@ -55,42 +57,137 @@ class MyPic extends StatelessWidget{
   }
 }
 
-class MyGrid extends StatelessWidget{
-  final List<String> urls;
-  const MyGrid(this.urls);
+class MyGrid extends StatefulWidget{
+  @override
+  MyGridState createState() => MyGridState();
+}
+
+class MyGridState extends State<MyGrid> {
+  List<String> origin = <String>[];
+  List<String> displayed = <String>[];
+
+  Future<String> fetchFileData() async{
+    return await rootBundle.loadString('txt/urls.txt');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchFileData().then((raw) {
+      setState(() {
+        this.origin = this.displayed = raw.split("|");
+      });
+    });
+  }
+
+  void reverse() {
+    setState(() {
+      this.displayed = List.from(this.origin.reversed);
+    });
+  }
+
+  void shuffle() {
+    int size = this.origin.length;
+    List<bool> visited = List<bool>.filled(size, false);
+    List<String> shuffled = <String>[];
+
+    Random randomObj = Random();
+    while(shuffled.length != size) {
+      int index = randomObj.nextInt(size);
+      if(visited[index] == false) {
+        shuffled.add(this.origin[index]);
+        visited[index] = true;
+      }
+    }
+
+    setState(() {
+      this.displayed = shuffled;
+    });
+  }
+
+  void reset() {
+    setState(() {
+      this.displayed = this.origin;
+    });
+  }
 
   @override
   Widget build(BuildContext context){
-    return SafeArea(
-      child: GridView.count(
-        primary: false,
-        padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 20.0),
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 20,
-        crossAxisCount: 2,
-        children: this.urls.asMap().entries.map((each) {
-          return Container(
-            child: MyPic(each.value, each.key),
-          );
-        }).toList(),
+    return Column(
+      children: <Widget>[
+        Expanded(
+          flex: 30,
+          child: GridView.count(
+              shrinkWrap: true,
+              primary: false,
+              padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 20.0),
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 20,
+              crossAxisCount: 2,
+              children: displayed.asMap().entries.map((each) {
+                return Container(
+                  child: MyPic(each.value, each.key),
+                );
+              }).toList()
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                  flex: 1,
+                  child: MyButton('Reverse', this.reverse)
+              ),
+              Expanded(
+                  flex: 1,
+                  child: MyButton('Shuffle', this.shuffle)
+              ),
+              Expanded(
+                  flex: 1,
+                  child: MyButton('Reset', this.reset)
+              )
+            ],
+          )
+        )
+      ],
+    );
+  }
+}
+
+typedef SwitchOrderCallback = Function();
+class MyButton extends StatelessWidget {
+  final String title;
+  final SwitchOrderCallback orderSwitch;
+  const MyButton(this.title, this.orderSwitch);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        this.orderSwitch();
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 5.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: Center(
+          child: Text(this.title),
+        ),
       ),
     );
   }
 }
 
-void main() async{
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  Future<String> fetchFileData() async{
-    return await rootBundle.loadString('txt/urls.txt');
-  }
-
-  String raw = await fetchFileData();
-  List<String> urls = raw.split("|");
   runApp(
     MaterialApp(
       home: Scaffold(
         body: Center(
-          child: MyGrid(urls)
+          child: MyGrid(),
         ),
       ),
     ),
